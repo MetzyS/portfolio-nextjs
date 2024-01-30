aa88c8ec39396d1eb25aeecbb9cf3c09bed8a0d2
metzys <contact@metzys.net>
2024-01-30 20:23:07 +0100

    Avatar Vogue Merry

--- src/components/Sections/About/About.tsx
+++ src/components/Sections/About/About.tsx
@@ -1,4 +1,5 @@
 import { AvatarArrow } from "@/components/AvatarArrow";
+import { AvatarBoat } from "@/components/AvatarBoat";
 import { AvatarCloud } from "@/components/AvatarCloud";
 import { AvatarHappy } from "@/components/AvatarHappy";
 import { AvatarTired } from "@/components/AvatarTired";
@@ -94,6 +95,60 @@ export const About = () => {
             />
           </RevealWrapper>
         </div>
+        <div className="relative hidden md:block">
+          <RevealWrapper
+            delay={150}
+            useDelay="always"
+            opacity={0}
+            reset={false}
+            className="load-hidden"
+          >
+            <AvatarTravel
+              width={200}
+              height={200}
+              className="absolute left-10 xl:left-4"
+            />
+          </RevealWrapper>
+          <RevealWrapper
+            delay={150}
+            useDelay="always"
+            opacity={0}
+            reset={false}
+            className="load-hidden"
+          >
+            <AvatarBoat
+              width={180}
+              height={180}
+              className="absolute top-56 xl:top-36 right-6 xl:right-0"
+            />
+          </RevealWrapper>
+        </div>
+        <p className="relative flex flex-col text-base leading-tight text-slate-800 dark:text-slate-300 dark:bg-slate-700/30 bg-sky-100/20 rounded-lg p-4 shadow-xl dark:md:shadow-slate-900 md:shadow-md overflow-hidden">
+          <span className="mb-2 md:mb-0 text-base leading-tight">
+            <strong>Ancien directeur d'ACM*</strong> avec plusieurs années
+            d'expérience dans le monde de l'animation socio-culturelle.
+          </span>
+          <span className="text-xs italic">*Accueil collectif de Mineurs.</span>
+          <span className="mt-6">
+            J'ai entammé une{" "}
+            <strong>
+              reconversion professionnelle dans le développement web{" "}
+            </strong>
+            en 2022, en raison de ma passion pour l'informatique, le code 👨🏽‍💻 et
+            les jeux vidéo 🎮.
+          </span>
+          <span className="mt-6">
+            Depuis, je passe mes journées{" "}
+            <span className="text-sm italic">(et parfois mes nuits!)</span> a
+            <strong> coder</strong>, <strong>apprendre et explorer</strong>{" "}
+            cette nouvelle facette d'internet.
+          </span>
+          <span>
+            C'est d'ailleurs ce qui me motive le plus.. l'
+            <strong>apprentissage</strong>! Le renouvellement et la remise en
+            question permanente dans un domaine qui évolue sans cesse.
+          </span>
+        </p>
       </div>
     </>
   );
